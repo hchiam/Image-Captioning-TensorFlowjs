3144b57e06382c42c6c0a2ffe87114d1a0dae835
hchiam <hchiam@users.noreply.github.com>
2024-06-06 22:05:00 -0600

    autoformat and try some logging

--- js/caption.js
+++ js/caption.js
@@ -1,4 +1,3 @@
-
 let img = document.querySelector("#image");
 let button = document.querySelector("#btn");
 let text = document.querySelector("#txt");
@@ -10,17 +9,17 @@ let isModelLoaded = false;
 let model;
 let mobileNet;
 
-const maxLen = 40; // 40
+const maxLen = 40;
 
 function preprocess(imgElement) {
-    return tf.tidy(() => {
-        let tensor = tf.fromPixels(imgElement).toFloat();
-        const resized = tf.image.resizeBilinear(tensor,[224,224]);
-        const offset = 125.5;
-        const normalized = resized.div(offset).sub(tf.scalar(1.0));
-        const batched = normalized.expandDims(0);
-        return batched;
-    });
+  return tf.tidy(() => {
+    let tensor = tf.fromPixels(imgElement).toFloat();
+    const resized = tf.image.resizeBilinear(tensor, [224, 224]);
+    const offset = 125.5;
+    const normalized = resized.div(offset).sub(tf.scalar(1.0));
+    const batched = normalized.expandDims(0);
+    return batched;
+  });
 }
 
 /* function asyncCaption(img) {
@@ -76,94 +75,97 @@ function preprocess(imgElement) {
 } */
 
 function caption(img) {
-    // should use promise and async-await to make it non blocking
-    // max_len change karna
-    let flattenLayer = tf.layers.flatten();
-    console.log("Inside caption()");
-    return tf.tidy(()=> {
+  // should use promise and async-await to make it non blocking
+  // max_len change karna
+  let flattenLayer = tf.layers.flatten();
+  console.log("Inside caption()");
+  return tf.tidy(() => {
+    let startWord = ["<start>"];
+    let idx, wordPred;
+    let e = flattenLayer.apply(mobileNet.predict(img));
 
-        let startWord = ['<start>'];
-        let idx,wordPred;
-        let e = flattenLayer.apply(mobileNet.predict(img));
-        
-        while (true) {
-            let parCaps = [];
-            for (let j = 0; j < startWord.length; ++j) {
-                //console.log(typeof(parCaps));
-                parCaps.push(word2idx[startWord[j]]);
-            }
-            parCaps = tf.tensor1d(parCaps)
-                        .pad([[0, maxLen - startWord.length]])
-                        .expandDims(0);
+    while (true) {
+      let parCaps = [];
+      for (let j = 0; j < startWord.length; ++j) {
+        //console.log(typeof(parCaps));
+        parCaps.push(word2idx[startWord[j]]);
+      }
+      parCaps = tf
+        .tensor1d(parCaps)
+        .pad([[0, maxLen - startWord.length]])
+        .expandDims(0);
 
-            
-            let preds = model.predict([e,parCaps]);
-            preds = preds.reshape([preds.shape[1]]);
-            //console.log(preds.shape);
-            
-            idx = preds.argMax().dataSync();
-            wordPred = idx2word[idx];
-            
-            startWord.push(wordPred);            
-            if(wordPred=='<end>'||startWord.length>maxLen)
-                break;
-        }
-        // removing first and last tokern <start> and <end>
-        startWord.shift();
-        startWord.pop();
-        
-        cap = startWord.join(' ');
-        console.log("caption: ", cap);
-        capField.innerHTML = cap;
-        //return startWord.join(' ');
-        //return asyncCaption(img);
-    }); 
+      let preds = model.predict([e, parCaps]);
+      preds = preds.reshape([preds.shape[1]]);
+      //console.log(preds.shape);
+
+      idx = preds.argMax().dataSync();
+      wordPred = idx2word[idx];
+
+      startWord.push(wordPred);
+      if (wordPred == "<end>" || startWord.length > maxLen) break;
+    }
+    // removing first and last tokern <start> and <end>
+    startWord.shift();
+    startWord.pop();
+
+    cap = startWord.join(" ");
+    console.log("caption: ", cap);
+    capField.innerText = cap;
+    //return startWord.join(' ');
+    //return asyncCaption(img);
+  });
 }
 
-
 async function start() {
-    //mobileNet = loadMobileNet();
-    const mobilenet = await tf.loadModel('https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json');
-    const layer = mobilenet.getLayer('conv_preds');
-    //console.log("mobileNet loaded");
-    mobileNet =  tf.model({
-        'inputs': mobilenet.inputs,
-        'outputs': layer.output
-    });
+  //mobileNet = loadMobileNet();
+  const mobilenet = await tf.loadModel(
+    "https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json"
+  );
+  const layer = mobilenet.getLayer("conv_preds");
+  console.log("mobileNet loaded");
+  mobileNet = tf.model({
+    inputs: mobilenet.inputs,
+    outputs: layer.output,
+  });
 
-    model = await tf.loadModel('model/model.json');
-    console.log("Inside start()");
-    modelLoaded();
+  model = await tf.loadModel("model/model.json");
+  console.log("Inside start()");
+  modelLoaded();
 }
 
 function modelLoaded() {
-    mobileNet.predict(tf.zeros([1, 224, 224, 3])).dispose();
-    //console.log("Inside modelLoaded()");
-    isModelLoaded = true;
-    text.innerHTML = "Models Loaded!";
+  mobileNet.predict(tf.zeros([1, 224, 224, 3])).dispose();
+  console.log("Inside modelLoaded()");
+  isModelLoaded = true;
+  text.innerText = "Models Loaded!";
 }
 
-
-imageLoader.addEventListener('change', function(e) {
+imageLoader.addEventListener(
+  "change",
+  function (e) {
     let reader = new FileReader();
-    reader.onload = function() {
-        img.src = reader.result;
-    }
-    reader.readAsDataURL(e.target.files[0]); 
-}, false);
+    reader.onload = function () {
+      img.src = reader.result;
+    };
+    reader.readAsDataURL(e.target.files[0]);
+  },
+  false
+);
 
-button.addEventListener("click",function() {
-    //console.log("button pressed");
-    if(!isModelLoaded) {
-        console.log('Models not loaded yet');
-        return;
-    }
-    capField.innerHTML = "Generating Caption ... Please Wait";
-    let picture = preprocess(img);
-    //let cap = caption(picture);
-    caption(picture);
-    //console.log("caption: "+ cap);
-    //capField.innerHTML = cap;
+button.addEventListener("click", function () {
+  console.log("button pressed");
+  if (!isModelLoaded) {
+    console.log("Models not loaded yet");
+    return;
+  }
+  alert("generating caption");
+  capField.innerText = "Generating Caption ... Please Wait"; // this line doesn't work?
+  let picture = preprocess(img);
+  //let cap = caption(picture);
+  caption(picture);
+  console.log("caption: " + cap);
+  //capField.innerText = cap;
 });
 
 start();
